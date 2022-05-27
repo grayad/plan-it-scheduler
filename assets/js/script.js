f740f6ed24db470136bd8561f369700d969119bd
Alexus Gray <alexus_gray@alumni.unc.edu>
2022-05-26 21:35:05 -0400

    update time colors for after 12AM

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,9 +10,30 @@ currentDayEl.innerText=currentDate;
 var currentTime = moment().format("hA");
 console.log(currentTime);
 
-// set colors according to time
+
+// set textarea colors according to time
 function timeColor(){
     switch (currentTime) {
+        case "12AM":
+        case "1AM":
+        case "2AM":
+        case "3AM":
+        case "4AM":
+        case "5AM":
+        case "6AM":
+        case "7AM":
+        case "8AM":
+            $("#hour9").addClass("future");
+            $("#hour10").addClass("future");
+            $("#hour11").addClass("future");
+            $("#hour12").addClass("future");
+            $("#hour1").addClass("future");
+            $("#hour2").addClass("future");
+            $("#hour3").addClass("future");
+            $("#hour4").addClass("future");
+            $("#hour5").addClass("future");
+            break;
+
         case "9AM":
             $("#hour9").addClass("present");
             $("#hour10").addClass("future");
@@ -120,14 +141,29 @@ function timeColor(){
             $("#hour4").addClass("past");
             $("#hour5").addClass("present");
             break;
+
+        default:
+            $("#hour9").addClass("past");
+            $("#hour10").addClass("past");
+            $("#hour11").addClass("past");
+            $("#hour12").addClass("past");
+            $("#hour1").addClass("past");
+            $("#hour2").addClass("past");
+            $("#hour3").addClass("past");
+            $("#hour4").addClass("past");
+            $("#hour5").addClass("past");
+            break;
     };
 };
 
-function saveBtnHandler() {
-    var userInput= inputEl.value;
-    console.log("button clicked");
-};
+// save inputs to localstorage when a save button is clicked
+function saveInputs9(){
+
+}
+
+// function saveBtnHandler() {
+//     var textValue= hour9.value.val().trim();
+//     console.log(textValue);
+// };
 
 timeColor();
-
-saveBtn.addEventListener("click", saveBtnHandler);
